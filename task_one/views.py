8b84278e59ee6b4c41314babfc916c708b89a451
noblet <appiahpatricknoblet@gmail.com>
2024-07-03 17:26:38 +0100

    added new config files

--- task_one/views.py
+++ task_one/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.http import JsonResponse
 import ip2locationio
 import requests
@@ -19,8 +18,10 @@ def hello(request):
         
         return JsonResponse(response_data)
 
+
 def get_location(ip):
-        configuration = ip2locationio.Configuration("ADB2AA9BE78EC4F52D61EDA4BB156F8A")
+        GEOLOCATION_API_KEY = "ADB2AA9BE78EC4F52D61EDA4BB156F8A"
+        configuration = ip2locationio.Configuration(GEOLOCATION_API_KEY)
         ipgeolocation = ip2locationio.IPGeolocation(configuration)
         
         try:
@@ -28,6 +29,7 @@ def get_location(ip):
                 return rec.get('city_name', 'Unknown Location')
         except Exception as e:
                 return 'Unknown Location'
+
 
 def get_weather(city):
         WEATHER_API_KEY = "5267f59717294cc8688c24d664ab19ca"
@@ -37,5 +39,3 @@ def get_weather(city):
                 return weather_data['main']['temp']
         except Exception as e:
                 return 'N/A'
-         
-        
