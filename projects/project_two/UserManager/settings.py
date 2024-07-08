8addc51e7936d87e0c547e9889340b311daf8de4
Patrick A. Noblet <appiahpatricknoblet@gmail.com>
2024-07-08 17:39:18 +0000

    Update settings.py

--- projects/project_two/UserManager/settings.py
+++ projects/project_two/UserManager/settings.py
@@ -38,6 +38,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
